0a10c8639817d830c014387e191feaceaeea56ad
jymin <jymin@kissco.co.jp>
2021-03-19 18:33:28 +0900

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  
+  const [lists, setLists] = useState([]);
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    axios.get('/api/values').then(response => {
+      console.log("response", response);
+      setLists(response.data);
+    });
+  }, []);
+
+  const changeHandler = (e) => {
+    setValue(e.currentTarget.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.post('/api/value', { value: value }).then(response => {
+      if (response.data.success) {
+        console.log('response', response);
+        setLists([...lists, response.data]);
+        setValue("");
+      } else {
+        alert('failed!');
+      }
+    })
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="container">
+          {
+            lists && lists.map((list, index) => (
+              <li key={index}>{list.value}</li>
+            ))
+          }
+          <br />
+          <form className="example" onSubmit={submitHandler}>
+            <input 
+              type="text"
+              placeholder="input..."
+              onChange={changeHandler}
+              value={value}
+            />
+            <button type="submit">OK</button>
+          </form>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
